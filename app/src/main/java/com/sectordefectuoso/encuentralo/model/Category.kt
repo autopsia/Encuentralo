3c647b3d43d2bf8fa79cfd383f525502b228de07
autopsia <autopsia9@gmail.com>
2020-05-17 12:10:16 -0500

    Agregar colores corporativos. Remover clases deprecadas.

---
+++ app/src/main/java/com/sectordefectuoso/encuentralo/model/Category.kt
@@ -0,0 +1,10 @@
+package com.sectordefectuoso.encuentralo.model
+
+class Category(
+    name:String,
+    position:Int
+){
+    override fun toString(): String {
+        return super.toString()
+    }
+}
