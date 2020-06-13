cc8119a50e07ed555172c6bde1e24634d2e141ab
Jordi <jordi.diaz04@hotmail.com>
2020-06-12 22:38:15 -0500

    Permite la actualización de la información del usuario

--- app/src/main/java/com/sectordefectuoso/encuentralo/data/repository/UserRepository.kt
+++ app/src/main/java/com/sectordefectuoso/encuentralo/data/repository/UserRepository.kt
@@ -2,6 +2,7 @@ package com.sectordefectuoso.encuentralo.data.repository
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 import com.sectordefectuoso.encuentralo.data.model.User
 import com.sectordefectuoso.encuentralo.utils.ResourceState
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -21,12 +22,44 @@ class UserRepository {
             offer(ResourceState.Success(snapshot!!.toObject(User::class.java)))
             exception?.let {
                 offer(ResourceState.Failed(it.message.toString()))
-                cancel(it.message.toString())
             }
         }
         awaitClose {
             subscription.remove()
             cancel()
         }
+    }
+
+    fun updateEmail(email: String): Flow<ResourceState<Boolean>> = callbackFlow {
+        userAuth?.updateEmail(email)?.addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                offer(ResourceState.Success(true))
+            } else{
+                offer(ResourceState.Failed("No se pudo actualizar el correo electrónico"))
+            }
+        }
+        awaitClose { cancel() }
+    }
+
+    fun updateInformation(user: User): Flow<ResourceState<Boolean>> = callbackFlow {
+        val userMap = hashMapOf(
+            "document" to user.document,
+            "names" to user.names,
+            "lastNames" to user.lastNames,
+            "birthdate" to user.birthdate,
+            "phone" to user.phone,
+            "email" to user.email
+        )
+
+        userRef.document(userAuth!!.uid).set(userMap, SetOptions.merge()).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                offer(ResourceState.Success(true))
+            }
+            else{
+                offer(ResourceState.Failed("No se pudo actualizar la información"))
+                cancel("No se pudo actualizar la información")
+            }
+        }
+        awaitClose { cancel() }
     }
 }
--- app/src/main/java/com/sectordefectuoso/encuentralo/ui/profile/edit/ProfileFragment.kt
+++ app/src/main/java/com/sectordefectuoso/encuentralo/ui/profile/edit/ProfileFragment.kt
@@ -1,17 +1,23 @@
 package com.sectordefectuoso.encuentralo.ui.profile.edit
 
 import android.app.AlertDialog
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.sectordefectuoso.encuentralo.R
 import com.sectordefectuoso.encuentralo.data.model.User
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.sectordefectuoso.encuentralo.utils.Functions
+import com.sectordefectuoso.encuentralo.utils.ResourceState
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_account.*
 import kotlinx.android.synthetic.main.fragment_profile.*
-import kotlinx.android.synthetic.main.fragment_register_user.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -21,8 +27,9 @@ class ProfileFragment : Fragment() {
         fun newInstance() = ProfileFragment()
     }
 
-    private lateinit var viewModel: ProfileViewModel
+    private val profileViewModel by lazy { ViewModelProvider(this).get(ProfileViewModel::class.java)}
     private lateinit var alertDialog: AlertDialog
+    private var userBundle: User = User()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,7 +41,7 @@ class ProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+
         loadInformation()
 
         txtAccountBirthdate.setOnClickListener{
@@ -43,19 +50,34 @@ class ProfileFragment : Fragment() {
 
         btnAccountSave.setOnClickListener {
             if(validate()){
+                val dateFormat = SimpleDateFormat("dd/MM/yyyy")
+                var user = User()
+                user.document = txtAccountDocument.text.toString()
+                user.names = txtAccountName.text.toString()
+                user.lastNames = txtAccountLastName.text.toString()
+                user.birthdate = dateFormat.parse(txtAccountBirthdate.text.toString())
+                user.phone = txtAccountPhone.text.toString()
+                user.email = txtAccountEmail.text.toString()
+                profileViewModel.user = user
 
+                if(user.email.equals(userBundle.email)){
+                    updateInformation()
+                }
+                else{
+                    updateEmail()
+                }
             }
         }
     }
 
     private fun loadInformation(){
-        var user = arguments?.get("user") as User
-        txtAccountDocument.setText(user.document)
-        txtAccountName.setText(user.names)
-        txtAccountLastName.setText(user.lastNames)
-        txtAccountBirthdate.setText(SimpleDateFormat("dd/MM/yyyy").format(user.birthdate))
-        txtAccountPhone.setText(user.phone)
-        txtAccountEmail.setText(user.email)
+        userBundle = arguments?.get("user") as User
+        txtAccountDocument.setText(userBundle.document)
+        txtAccountName.setText(userBundle.names)
+        txtAccountLastName.setText(userBundle.lastNames)
+        txtAccountBirthdate.setText(SimpleDateFormat("dd/MM/yyyy").format(userBundle.birthdate))
+        txtAccountPhone.setText(userBundle.phone)
+        txtAccountEmail.setText(userBundle.email)
     }
 
     private fun validate(): Boolean{
@@ -86,5 +108,37 @@ class ProfileFragment : Fragment() {
         return false;
     }
 
-    
+    private fun updateEmail() {
+        profileViewModel.updateEmail.observe(viewLifecycleOwner, Observer{ result ->
+            when(result){
+                is ResourceState.Loading -> {
+                    Log.i("observable ", "Cargando")
+                }
+                is ResourceState.Success -> {
+                    updateInformation()
+                }
+                is ResourceState.Failed -> {
+                    Log.i("failure", "error")
+
+                }
+            }
+        })
+    }
+
+    private fun updateInformation() {
+        profileViewModel.updateInformation.observe(viewLifecycleOwner, Observer{ result ->
+            when(result){
+                is ResourceState.Loading -> {
+                    Log.i("observable ", "Cargando")
+                }
+                is ResourceState.Success -> {
+                    nav_host_fragment.findNavController().popBackStack(R.id.navigation_account, false)
+                }
+                is ResourceState.Failed -> {
+                    Log.i("failure", "error")
+
+                }
+            }
+        })
+    }
 }
