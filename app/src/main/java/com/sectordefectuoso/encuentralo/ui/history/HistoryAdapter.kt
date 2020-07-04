05c7d6552e043bdfbeb94dcf30e1bce587106bee
Jordi <jordi.diaz04@hotmail.com>
2020-07-04 07:56:57 -0500

    Chat en historial

--- app/src/main/java/com/sectordefectuoso/encuentralo/ui/history/HistoryAdapter.kt
+++ app/src/main/java/com/sectordefectuoso/encuentralo/ui/history/HistoryAdapter.kt
@@ -5,13 +5,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
 import com.sectordefectuoso.encuentralo.R
 import com.sectordefectuoso.encuentralo.data.model.Service
+import com.sectordefectuoso.encuentralo.ui.home.postlist.PostListFragmentDirections
 
 class HistoryAdapter(private val services: ArrayList<Service>): RecyclerView.Adapter<HistoryAdapter.MyViewHolder>() {
+    val auth = FirebaseAuth.getInstance().currentUser
+
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var lblTitle = itemView.findViewById<TextView>(R.id.lblHistoryTitle)
         var lblDate = itemView.findViewById<TextView>(R.id.lblHistoryDate)
@@ -33,6 +38,10 @@ class HistoryAdapter(private val services: ArrayList<Service>): RecyclerView.Ada
         val service = services[position]
         holder.lblTitle.text = service.title
         holder.lblDate.text = service.description
+        holder.btnChat.setOnClickListener {
+            val action = HistoryFragmentDirections.actionNavigationHistoryToChatFragment(service.documentId, service.title, auth!!.uid)
+            holder.itemView.findNavController().navigate(action)
+        }
 
         FirebaseStorage.getInstance().reference.child("User/${service.userId}.jpg").downloadUrl.addOnSuccessListener { uri ->
             val result = uri.toString()
